4b2035e99f52e05e7abf12552fee3354d47adba7
123Balramkumar <balramkgg99@gmail.com>
2023-07-16 17:12:35 +0530

    Task Management App Done

--- Task.js
+++ Task.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const taskDescriptionInput = document.getElementById('task-description');
     const createdDateInput = document.getElementById('created-date');
     const taskList = document.getElementById('task-list');
+    const noTasks = document.getElementById('noTasks') 
   
     taskForm.addEventListener('submit', function(event) {
       event.preventDefault(); // Prevent form submission
@@ -24,16 +25,34 @@ document.addEventListener('DOMContentLoaded', function() {
       const taskCard = document.createElement('li');
       taskCard.className = 'task-card';
       taskCard.innerHTML = `
+      <div class="mainTaskListContainner">
         <h3>Title: ${title}</h3>
         <p>Description: ${description}</p>
-        <small>Created on ${date}</small>
-        <button class="delete-button">Delete</button>
+        <p>Created on ${date}</p>
+        <div class="checkox">
+        <div>
+        <input type="checkbox" class="checkbox" id="checkbox-${Date.now()}">
+        <label for="checkbox-${Date.now()}">Completed</label>
+      </div>
+        <button  class="delete-button">Delete</button>
+        </div>
+<div>
       `;
       taskList.appendChild(taskCard);
+
+      var task = noTasks.classList.contains('noTasks')
+      if (task) {
+        noTasks.classList.remove('noTasks')
+        noTasks.classList.add('noNone')
+      }
   
       const deleteButton = taskCard.querySelector('.delete-button');
       deleteButton.addEventListener('click', function() {
         taskCard.remove();
+      });
+      const checkbox = taskCard.querySelector('.checkbox');
+      checkbox.addEventListener('change', function() {
+        taskCard.classList.toggle('completed');
       });
     }
   });
